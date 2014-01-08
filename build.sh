bf71f390c2ddf16c60e2d2a3763efd75eeee9248
Sarah Tappon <sarah.tappon@gmail.com>
2014-01-08 14:07:59 -0800

    link to our freetype version

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
-./configure
+export FT2_CONFIG_DIR=`(cd ../freetype2/builds/unix && pwd)`
+export FT2_CONFIG=$FT2_CONFIG_DIR/freetype-config
+mkdir -p build
+cd build
+../configure --disable-freetypetest --srcdir=.. CXX=clang++ CC=clang CPPFLAGS=-I`(cd ../../freetype2/include && pwd)` LDFLAGS=-L`(cd ../../freetype2/objs/.libs && pwd)`
 make
